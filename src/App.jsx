4ed32c63eee3ec0641ee08509bbfea41c2bdf924
chxpz <rpcweb3@gmail.com>
2024-01-29 09:41:33 -0300

    chore: reducing state management

--- src/App.jsx
+++ src/App.jsx
@@ -4,34 +4,38 @@ import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
 import Log from "./components/Log";
 
+const deriveActivePlayer = (gameTurns) => {
+  let currentPlayer = "X";
+
+  if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+    currentPlayer = "O";
+  }
+
+  return currentPlayer;
+};
+
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState("X");
+
+  const activePlayer = deriveActivePlayer(gameTurns);
 
   const handleSelectSquare = (rowIndex, colIndex) => {
-    setActivePlayer((currentActivePlayer) =>
-      currentActivePlayer === "X" ? "O" : "X"
-    ),
-      setGameTurns((prevTurns) => {
-        let currentPlayer = "X";
+    setGameTurns((prevTurns) => {
+      const currentPlayer = deriveActivePlayer(prevTurns);
 
-        if (prevTurns.length > 0 && prevTurns[0].player === "X") {
-          currentPlayer = "O";
-        }
-
-        const updatedTurns = [
-          {
-            square: {
-              row: rowIndex,
-              col: colIndex,
-            },
-            player: currentPlayer,
+      const updatedTurns = [
+        {
+          square: {
+            row: rowIndex,
+            col: colIndex,
           },
-          ...prevTurns,
-        ];
+          player: currentPlayer,
+        },
+        ...prevTurns,
+      ];
 
-        return updatedTurns;
-      });
+      return updatedTurns;
+    });
   };
 
   return (
